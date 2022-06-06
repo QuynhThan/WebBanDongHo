09d1a236edf78b68f50a4449f9a798f2accf84bf
quynh <thanngocquynh1101@gmail.com>
2022-06-06 15:40:23 +0700

    initial commit

---
+++ src/webdongho/entity/PHIEUDAT__.java
@@ -0,0 +1,157 @@
+package webdongho.entity;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinColumns;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table (name = "PHIEUDAT")
+public class PHIEUDAT__ {
+	@Id 
+	@Column(name = "MAPD")
+	private String MAPD;
+	
+	@Column(name = "NGAYDAT")
+	private Date NGAYDAT;
+	
+	@Column(name = "HOTENNGUOINHAN", columnDefinition = "nvarchar(50)")
+	private String HOTENNGUOINHAN;
+	
+	@Column(name = "DIACHI", columnDefinition = "nvarchar(100)")
+	private String DIACHI;
+	
+	@Column(name = "SDT")
+	private String SDT;
+	
+	@Column(name = "NGAYGIAO")
+	private Date NGAYGiAO;
+	
+	@Column(name = "TRANGTHAI")
+	private int TRANGTHAI;
+	
+	@ManyToOne
+	@JoinColumn(name = "MANV")
+	private NHANVIEN MANV_PD;
+
+	@ManyToOne
+	@JoinColumn(name = "MAKH")
+	private KHACHHANG MAKH_PD;
+	
+
+	@OneToMany(mappedBy = "PD_CTPD",fetch = FetchType.EAGER)
+	private Collection<CTPD__> CTPDS_PD;
+	
+
+	public String getMAPD() {
+		return MAPD;
+	}
+
+	public void setMAPD(String mAPD) {
+		MAPD = mAPD;
+	}
+
+	public Date getNGAYDAT() {
+		return NGAYDAT;
+	}
+
+	public void setNGAYDAT(Date nGAYDAT) {
+		NGAYDAT = nGAYDAT;
+	}
+
+	public String getHOTENNGUOINHAN() {
+		return HOTENNGUOINHAN;
+	}
+
+	public void setHOTENNGUOINHAN(String hOTENNGUOINHAN) {
+		HOTENNGUOINHAN = hOTENNGUOINHAN;
+	}
+
+	public String getDIACHI() {
+		return DIACHI;
+	}
+
+	public void setDIACHI(String dIACHI) {
+		DIACHI = dIACHI;
+	}
+
+	public String getSDT() {
+		return SDT;
+	}
+
+	public void setSDT(String sDT) {
+		SDT = sDT;
+	}
+
+	public Date getNGAYGiAO() {
+		return NGAYGiAO;
+	}
+
+	public void setNGAYGiAO(Date nGAYGiAO) {
+		NGAYGiAO = nGAYGiAO;
+	}
+
+	public int getTRANGTHAI() {
+		return TRANGTHAI;
+	}
+
+	public void setTRANGTHAI(int tRANGTHAI) {
+		TRANGTHAI = tRANGTHAI;
+	}
+
+	
+
+	public KHACHHANG getMAKH_PD() {
+		return MAKH_PD;
+	}
+
+	public void setMAKH_PD(KHACHHANG mAKH_PD) {
+		MAKH_PD = mAKH_PD;
+	}
+
+	public Collection<CTPD__> getCTPDS_PD() {
+		Collection<CTPD__> ctpds = CTPDS_PD;
+		List listctpd = new ArrayList<CTPD__>(CTPDS_PD);
+			for (int i = 0;i < listctpd.size(); ) {
+				for (int j = i + 1; j < listctpd.size() ; j++) {
+					if(listctpd.get(i).equals(listctpd.get(j))) {
+						ctpds.remove(listctpd.get(i));
+						listctpd.remove(i);
+						i--;
+						break;
+					}
+				}
+				i++;
+			}
+		
+		
+		return ctpds;
+	}
+
+	public void setCTPDS_PD(Collection<CTPD__> cTPDS_PD) {
+		CTPDS_PD = cTPDS_PD;
+	}
+
+	
+	public NHANVIEN getMANV_PD() {
+		return MANV_PD;
+	}
+
+	public void setMANV_PD(NHANVIEN mANV_PD) {
+		MANV_PD = mANV_PD;
+	}
+
+	
+	
+
+}
